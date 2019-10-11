e738c259f489382acf7ac2fd80ec519ee83dce3e
fugashy <fugashy@icloud.com>
2019-10-12 01:58:55 +0900

    Add cv bridging to io

--- src/image_proc_chain/io.cpp
+++ src/image_proc_chain/io.cpp
@@ -1,6 +1,8 @@
 #include "image_proc_chain/io.hpp"
 
-#include <iostream>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.hpp>
+
 
 using std::placeholders::_1;
 
@@ -14,7 +16,18 @@ IO::IO(const std::string& node_name) : Node(node_name) {
 }
 
 void IO::Process(const sensor_msgs::msg::Image::SharedPtr msg) {
-  std::cout << "hello" << std::endl;
+  RCLCPP_INFO(get_logger(), "subscription event has come to");
+
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  } catch (cv_bridge::Exception& e) {
+    RCLCPP_ERROR(get_logger(), "cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  RCLCPP_INFO(get_logger(), "%d %d", cv_ptr->image.rows, cv_ptr->image.cols);
+
   pub_->publish(*msg);
 }
 
