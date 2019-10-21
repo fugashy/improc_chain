2aca1f37f1d404f7adff216e081a0573744a3199
fugashy <fugashy@icloud.com>
2019-10-21 22:09:46 +0900

    Add chain processor node

---
+++ src/image_proc_chain/chain_processor_node.cpp
@@ -0,0 +1,13 @@
+#include "image_proc_chain/chain_processor.hpp"
+
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("chain_processor");
+  auto cp = std::make_shared<image_proc_chain::ChainProcessor>(node);
+
+  cp->get_executor()->spin();
+  rclcpp::shutdown();
+
+  return EXIT_SUCCESS;
+}
