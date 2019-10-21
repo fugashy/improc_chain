0bd65919d5c196ae859624fe7190b694495283aa
fugashy <fugashy@icloud.com>
2019-10-21 22:35:42 +0900

    Extract FlexibleChainExecutor as a file

--- include/image_proc_chain/chain_processor.hpp
+++ include/image_proc_chain/chain_processor.hpp
@@ -1,32 +1,10 @@
 #ifndef IMAGE_PROC_CHAIN_CHAIN_PROCESSOR_HPP_
 #define IMAGE_PROC_CHAIN_CHAIN_PROCESSOR_HPP_
-
-#include "image_proc_chain/chain_piece.hpp"
+#include "image_proc_chain/flexible_chain_executor.hpp"
 #include "image_proc_chain/srv/change_chain_num.hpp"
 
 
 namespace image_proc_chain {
-
-class FlexibleChainExecutor {
- public:
-  using SharedPtr = std::shared_ptr<FlexibleChainExecutor>;
-
-  explicit FlexibleChainExecutor(rclcpp::Node::SharedPtr& node, const uint32_t index);
-  virtual ~FlexibleChainExecutor() = default;
-
-  bool ChangeNumTo(const uint32_t num);
-
-  rclcpp::executors::SingleThreadedExecutor::SharedPtr get_executor() const { return executor_; }
-
- private:
-  rclcpp::Node::SharedPtr node_;
-
-  std::vector<rclcpp::Node::SharedPtr> piece_nodes_;
-  std::vector<ChainPiece::SharedPtr> chain_pieces_;
-
-  rclcpp::executors::SingleThreadedExecutor::SharedPtr executor_;
-};
-
 
 class ChainProcessor {
  public:
---
+++ include/image_proc_chain/flexible_chain_executor.hpp
@@ -0,0 +1,28 @@
+#ifndef IMAGE_PROC_CHAIN_FLEXIBLE_CHAIN_EXECUTOR_HPP_
+#define IMAGE_PROC_CHAIN_FLEXIBLE_CHAIN_EXECUTOR_HPP_
+#include "image_proc_chain/chain_piece.hpp"
+
+namespace image_proc_chain {
+
+class FlexibleChainExecutor {
+ public:
+  using SharedPtr = std::shared_ptr<FlexibleChainExecutor>;
+
+  explicit FlexibleChainExecutor(rclcpp::Node::SharedPtr& node, const uint32_t index);
+  virtual ~FlexibleChainExecutor() = default;
+
+  bool ChangeNumTo(const uint32_t num);
+
+  rclcpp::executors::SingleThreadedExecutor::SharedPtr get_executor() const { return executor_; }
+
+ private:
+  rclcpp::Node::SharedPtr node_;
+
+  std::vector<rclcpp::Node::SharedPtr> piece_nodes_;
+  std::vector<ChainPiece::SharedPtr> chain_pieces_;
+
+  rclcpp::executors::SingleThreadedExecutor::SharedPtr executor_;
+};
+
+}
+#endif
